4a655d924bf952c30a7855ae5f065b8878e358de
RAHUL RUDRA <rahulrudra245@gmail.com>
2022-02-28 10:43:05 +0600

    fddirccst commit

---
+++ script.js
@@ -0,0 +1,11 @@
+let inputFieldSelect = document.getElementById('input-field');
+const inputButtonSelect = document.getElementById('input-button');
+
+
+const getPlayers = () => {
+    console.log(inputFieldSelect.value);
+
+    inputFieldSelect.value = ' ';
+    
+    
+}
