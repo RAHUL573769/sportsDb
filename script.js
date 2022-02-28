23f1a6847e92c9863811fbdc02467d621558f6c2
RAHUL RUDRA <rahulrudra245@gmail.com>
2022-02-28 11:16:12 +0600

    fddcccccccirccst commit

--- script.js
+++ script.js
@@ -27,6 +27,22 @@ const displayPlayerDetail = (players) => {
 
     for ( const player of players) {
         console.log(player);
+        const parent = document.getElementById('player-card');
+        const insideParentDiv = document.createElement('div');
+        insideParentDiv.innerHTML=` <div class="card">
+              <div class="prof-pic">
+                <img src="" alt="" />
+              </div>
+              <h2>Name::Rahul </h2>
+              <h5>Country:</h5>
+              <p></p>
+              <div class="all-button">
+                <div class="btn btn-danger">Delete</div>
+                <div class="btn btn-success">Details</div>
+              </div>
+            </div>`
+        parent.appendChild(insideParentDiv);
+
     }
 
     
