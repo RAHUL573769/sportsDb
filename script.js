3dc340cf039479bcb252dee4ab2a701b098f9ab7
RAHUL RUDRA <rahulrudra245@gmail.com>
2022-02-28 11:10:50 +0600

    fddcccccirccst commit

--- script.js
+++ script.js
@@ -3,9 +3,31 @@ const inputButtonSelect = document.getElementById('input-button');
 
 
 const getPlayers = () => {
-    console.log(inputFieldSelect.value);
 
-    inputFieldSelect.value = ' ';
+     inputFieldSelect.value = ' ';
+    const url = `https://www.thesportsdb.com/api/v1/json/2/searchplayers.php?p=${inputFieldSelect.value}`;
+
+    fetch(url)
+        .then(data => data.json())
+        .then(data => displayPlayerDetail(data.player))
     
+
+    // console.log(url);
+ 
+
+   
+    
+    
+}
+
+
+const displayPlayerDetail = (players) => {
+
+    // console.log(players);
+
+    for ( const player of players) {
+        console.log(player);
+    }
+
     
 }
